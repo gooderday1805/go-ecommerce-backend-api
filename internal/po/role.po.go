45e56915bea9a66381bbef16996c82a06484df62
taivan1805 <taivan1805@gmail.com>
2025-05-01 17:58:38 +0700

    feat: define User and Role models with associations
    - Add User model with fields UUID, UserName, IsActive.
    - Establish many-to-many relationship between User and Role.
    - Create Role model with fields ID, RoleName, RoleNote.
    - Set custom table names for User and Role models (go_db_user, go_db_role).

--- internal/po/role.po.go
+++ internal/po/role.po.go
@@ -1,17 +1,16 @@
 package po
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Role struct {
-	gorm.Model
-	ID       uuid.UUID `gorm:"column:id;type:int;not null;primaryKey;comment:ID"`
-	RoleName string    `gorm:"column:role_name"`
-	RoleNote string    `gorm:"column:role_note"`
+    gorm.Model
+    ID       int64  `gorm:"column:id; type:int; not null; primaryKey; autoIncrement; comment:'primary Key is ID'"`
+    RoleName string `gorm:"column:role_name"`
+    RoleNote string `gorm:"column:role_note; type:text;"`
 }
 
-func (u *Role) TableName() string {
-	return "go_db_user"
+func (r *Role) TableName() string {
+	return "go_db_role"
 }
